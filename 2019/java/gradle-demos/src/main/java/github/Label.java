cbb88dde9dec8d31bb104c8c5d9a9ef1043ab540
dtopuzov <dtopuzov@gmail.com>
2019-11-05 22:07:41 +0200

    chore: add github tests

---
+++ 2019/java/gradle-demos/src/main/java/github/Label.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Label {
+
+    @SerializedName("id")
+    @Expose
+    public Integer id;
+    @SerializedName("node_id")
+    @Expose
+    public String nodeId;
+    @SerializedName("url")
+    @Expose
+    public String url;
+    @SerializedName("name")
+    @Expose
+    public String name;
+    @SerializedName("color")
+    @Expose
+    public String color;
+    @SerializedName("default")
+    @Expose
+    public Boolean _default;
+
+}
